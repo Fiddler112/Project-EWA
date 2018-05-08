fe8e8fdd1de8db13631367bea91ea731992ca321
Unknown <perryvanede@gmail.com>
2018-05-08 13:11:42 +0200

    Added cookies, removed reduntant code, google log in functionality implemented

--- login.php
+++ login.php
@@ -1,14 +1,17 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-signin-client_id" content="307112913485-5kkslq098hfj65e6l3qngjo1916a7h4i.apps.googleusercontent.com">
-
+    <script> src="https://apis.google.com/js/platform.js" async defer>  </script> 
 	<title>Google Home - Login</title>
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/styles.css" rel="stylesheet">
-
+<!--	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet"> -->
+    
 </head>
 <body>
 	<div class="row">
@@ -29,8 +32,8 @@
 									<input name="remember" type="checkbox" value="Remember Me">Remember Me
 								</label>
 							</div>
-                            <div class="g-signin2" data-onsuccess="onSignIn" href="index.php">   
-                             </div>&nbsp;
+                            <div class="g-signin2" data-onsuccess="onSignIn" href="index.php"> </div>  
+                            
                             <button type="submit" class="btn btn-success" >Login</button>
 					</form>
 				</div>
@@ -39,26 +42,51 @@
 	</div><!-- /.row -->	
 	
 
-    <script src="https://apis.google.com/js/platform.js" async defer>   
-            
+    
+    <script>         
         function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+          
+    // The ID token you need to pass to your backend:
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+             var name = profile.getName();
+             var email=profile.getEmail();
+             var gIdToken = googleUser.getAuthResponse().id_token;
             
-            
-               // The ID token you need to pass to your backend:
-        var id_token = googleUser.getAuthResponse().id_token;
-        console.log("ID Token: " + id_token);
+          //Call create cookie script
+             createCookie('name', name, 30);
+             createCookie('email', email, 30);
+             createCookie('gIdToken', gIdToken, 30);
+        
+    //redirect to database to create user profile
+             window.location = "db_login_gUser.php?name=" + name + "&email=" + email + "&id_token=" + gIdToken;
+        
+      
+       
       };
-}
+       function createCookie(name,value, days){
+        var expires = "";
+        if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires;
+   
+       }
     
     </script>
     
     
 <script src="js/jquery-1.11.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+    
+    
+    
 </body>
 </html>

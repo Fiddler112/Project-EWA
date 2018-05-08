82742b10f0b5f744e4f642f77c1d6d5ef8d4e370
Wyomi Beuker <wyomi.beuker@hva.nl>
2018-05-08 11:26:06 +0200

    no message

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,3 @@
 <?php
-define('DB_NAME', 'zedep002');
-define('DB_USER', 'edep002');
-define('DB_PASSWORD','oMaIEqzNzZ$Fu/');
-define('DB_HOST', 'oege.ie.hva.nl:3306');
-$conn=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-// Check connection
-if (!$conn) {
-    die('Could not connect: ' . mysql_error());
-}
+
 ?>

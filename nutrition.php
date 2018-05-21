cd0b8ba43f6df115a29ed6ee5ea4199fb1150adb
Unknown <perryvanede@gmail.com>
2018-05-21 17:17:28 +0200

    Revert "Fixed errors on nutrition page when there was no search input"
    This reverts commit 34025feda31844be9b35dd8dab521889d88b38b7.

--- nutrition.php
+++ nutrition.php
@@ -171,14 +171,13 @@
 						</form>
                         
                             <?php
-                                   $name = $_POST["name"] ?? 'null';
-                                   $maxcalories = $_POST["maxCalories"] ?? '0'; 
-                        
+                                $name = $_POST["name"];
+                                $maxcalories = $_POST["maxCalories"];
                                 require __DIR__ . '/vendor/autoload.php';
                                 use RapidApi\RapidApiConnect;
                                 $rapid = new RapidApiConnect('default-application_5adf253de4b0b4824e5ac536', 'dc6004e0-4602-4c1c-b599-38838972f5ea');
 
-                                $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?query=".($name = $name ?? "")."&maxCalories=".($maxcalories =$maxcalories ?? "0") ,
+                                $response = Unirest\Request::get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?query=".$name."&maxCalories=".$maxcalories,
                               array(
                                 "X-Mashape-Key" => "1kEqiAEoRFmshiBb6AVUoeX6KvFNp1u8cndjsnSFvVG8zg3A1o",
                                 "X-Mashape-Host" => "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
@@ -187,17 +186,17 @@
                          );
                                 $getResponseVal = $response->raw_body;
                                 $getDecodeData = json_decode($getResponseVal, true);
-                               if(isset($getDecodeData['results'])){ 
-                                 foreach($getDecodeData['results'] as $key=>$value) {
+                                //$getSpecificValue = $getDecodeData['results'][0]['title'];
+                                
+                                foreach($getDecodeData['results'] as $key=>$value) {
                                     if (empty($value)) {
-                                        echo "<div class='panel-heading'>No recipes found </div>";
+                                        echo "<div class='panel-heading'>No recepts found </div>";
                                     } else {
                                         echo "<div class='panel-heading'>";
                                         echo "Recipe ".$key." : ".$value['title']."Amount of calories: ".$value['calories']. "<br>";
                                         echo "</div>";
                                         }
                                 }
-                            } 
                         ?>
 					</div>
 				</div>
